99e5145db1e403370ff0709c0ce41ebc840bd087
Colin Manning <colin.manning@me.com>
2013-07-08 14:01:04 +0200

    Latest fixes

--- disservice/disservice/src/com/setantamedia/dis/service/DisRequestHandler.java
+++ disservice/disservice/src/com/setantamedia/dis/service/DisRequestHandler.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 import com.setantamedia.dis.service.requests.LoginRequest;
 
@@ -28,17 +30,19 @@ public class DisRequestHandler extends Thread {
     * version of the string.
     */
    public void run() {
+      BufferedReader in = null;
+      PrintWriter out = null;
       try {
 
          /*
           * Decorate the streams so we can send characters and not just bytes.
           * Ensure output is flushed after every newline.
           */
-         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+         out = new PrintWriter(socket.getOutputStream(), true);
 
          // Send a welcome message to the client.
-         out.println(DisService.MESSAGE_READY);
+         // out.println(DisService.MESSAGE_READY);
 
          /*
           * For now we do not handle multi-line input String input = ""; while
@@ -65,19 +69,93 @@ public class DisRequestHandler extends Thread {
             }
             DisSession session = disService.doLogin(loginRequest);
             if (session != null) {
-               out.println(DisService.STATUS_OK + ":" + DisService.PARAMETER_SESSIONID + session.getId());
+               out.println(DisService.STATUS_OK + ":" + DisService.PARAMETER_SESSIONID + "=" + session.getId());
             } else {
                out.println(DisService.STATUS_NOT_AUTHORISED);
             }
             break;
          case DisService.OPERATION_LOGOUT:
+            String sessionId = null;
+            for (String commandPart : commandParts) {
+               String[] argParts = commandPart.split("=");
+               if (argParts.length == 2) {
+                  switch (argParts[0]) {
+                  case DisService.PARAMETER_SESSIONID:
+                     sessionId = argParts[1];
+                     ;
+                     break;
+                  }
+               }
+            }
+            String returnStatus = disService.doLogout(sessionId);
+            if (sessionId != null) {
+               out.println(returnStatus);
+            } else {
+               out.println(DisService.STATUS_NOT_AUTHORISED);
+            }
             break;
          case DisService.OPERATION_RUN_ACTION:
+            String actionName = null;
+            sessionId = null;
+            HashMap<String, String> params = new HashMap<>();
+            for (String commandPart : commandParts) {
+               String[] argParts = commandPart.split("=");
+               if (argParts.length == 2) {
+                  switch (argParts[0]) {
+                  case DisService.PARAMETER_SESSIONID:
+                     sessionId = argParts[1];
+                     break;
+                  case DisService.PARAMETER_NAME:
+                     actionName = argParts[1];
+                     break;
+                  default:
+                     params.put(argParts[0], argParts[1]);
+                     break;
+                  }
+               }
+            }
+            JSONObject result = disService.doRunAction(sessionId, actionName, params);
+            if (result == null) {
+               result = new JSONObject();
+            }
+            out.println(result);
+            break;
+         case DisService.OPERATION_DESCRIBE:
+            sessionId = null;
+            String viewName = null;
+            String outputFormat = null;
+            String connectionName = null;
+            for (String commandPart : commandParts) {
+               String[] argParts = commandPart.split("=");
+               if (argParts.length == 2) {
+                  switch (argParts[0]) {
+                  case DisService.PARAMETER_SESSIONID:
+                     sessionId = argParts[1];
+                     break;
+                  case DisService.PARAMETER_VIEW:
+                     viewName = argParts[1];
+                     break;
+                  case DisService.PARAMETER_FORMAT:
+                     outputFormat = argParts[1];
+                     break;
+                  case DisService.PARAMETER_CONNECTION:
+                     connectionName = argParts[1];
+                  default:
+                     break;
+                  }
+               }
+            }
+            result = disService.doDescribe(sessionId, connectionName, viewName, outputFormat);
+            if (result == null) {
+               result = new JSONObject();
+            }
+            out.println(result);
             break;
          default:
             logger.error("Ignoring invalid DIS operation '" + input + "'");
          }
       } catch (IOException e) {
+         out.println(DisService.STATUS_ERROR + ":" + e.getMessage());
          logger.debug("Error handling request");
          e.printStackTrace();
       } finally {
